a3eff3484f7777bee8376d094f4eb4130dcc4628
Ayush Kumar <44066050+quasayush@users.noreply.github.com>
2020-10-22 19:37:19 +0530

    Update grammar.c

--- Code/grammar.c
+++ Code/grammar.c
@@ -1,1 +1,20 @@
 #include "grammar.h"
+
+void grammar()
+{
+	FILE * fp = fopen("Grammar.txt", "r");
+	if(fp==NULL)
+	{
+		perror("Grammar file not found");
+		exit(0);
+	}
+
+	char str[100];
+	char* token;
+	char* rest;
+	char* lhsvalue;
+	LHS* lhs;
+	RHS* rhs;
+
+
+}
